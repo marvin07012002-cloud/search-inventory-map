f1a5f6637687fc875c80a361e2daadf8f3320793
marbin701 <marvin07012002@gmail.com>
2026-04-22 17:00:47 -0400

    trying to make my code work

--- src/main/java/Products.java
+++ src/main/java/Products.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
 
 public class Products {
+    private int id;
+    private String name;
+    private double price;
+
+
+
+    public Products(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+   /* HashMap<String, Products> inventory;
+
+        inventory.put(name, product);
+    String[] parts = input.split("\\|");
+
+    int id = Integer.parseInt(parts[0]);
+    String name = parts[1];
+    float price = Float.parseFloat(parts[2]);
+
+    Products product = new Products(id, name, price);
+
+        inventory.put(name, product);*/
+
+
+
+
 }
--- src/main/java/SearchInventoryMap.java
+++ src/main/java/SearchInventoryMap.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+import javax.xml.namespace.QName;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class SearchInventoryMap {
+
+    public static HashMap<String, Products> inventory = new HashMap<>();
+
+    public static void main(String[] args) {
+
+        loadInventory();
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Enter a product name: ");
+        String searchName = scanner.nextLine();
+
+        Products foundProduct = inventory.get(searchName);
+
+        if (foundProduct != null) {
+            System.out.printf("%d %s $%.2f%n", foundProduct.getId(), foundProduct.getName(), foundProduct.getPrice());
+        } else {
+            System.err.println("Product no found.");
+
+        }
+        scanner.close();
+
+    }
+
+    public static void loadInventory() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("inventory.csv"));
+            String input;
+
+            while((input = reader.readLine())!=null){
+                String[] parts = input.split("\\|");
+
+                int id = Integer.parseInt(parts[0]);
+                String name = parts[1];
+                double price = Double.parseDouble(parts[2]);
+
+                Products product = new Products (id,name,price);
+
+                inventory.put(name,product);
+            }
+            reader.close();
+
+        }catch (IOException e) {
+            System.err.println("Error Reading the inventory file for reason.");
+        }
+    }
 }
